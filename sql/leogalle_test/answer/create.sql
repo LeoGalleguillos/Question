e3ee7ea597e39af57074a577be036fc265933576
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-23 19:41:18 +0000

    Add name column to answer table

--- sql/leogalle_test/answer/create.sql
+++ sql/leogalle_test/answer/create.sql
@@ -2,6 +2,7 @@ CREATE TABLE `answer` (
     `answer_id` int(10) unsigned auto_increment,
     `question_id` int(10) unsigned not null,
     `user_id` int(10) unsigned default null,
+    `name` varchar(255) default null,
     `message` text,
     `created` datetime not null,
     PRIMARY KEY (`answer_id`),
