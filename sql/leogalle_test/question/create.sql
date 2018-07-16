111451d11a24fd409f5b362ea1a8924ee898773e
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-07-16 11:54:36 +0000

    Add name column to question table

--- sql/leogalle_test/question/create.sql
+++ sql/leogalle_test/question/create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `question` (
     `question_id` int(10) unsigned auto_increment,
     `user_id` int(10) unsigned default null,
+    `name` varchar(255) default null,
     `subject` varchar(255) not null,
     `message` text,
     `views` int(10) unsigned NOT NULL DEFAULT '0',
