288c8fa6d6acecf67f66fc874ec8a9c008492850
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-08-23 21:03:29 +0000

    Write and test question_meta table model

---
+++ src/Model/Table/QuestionMeta.php
@@ -0,0 +1,38 @@
+<?php
+namespace LeoGalleguillos\Question\Model\Table;
+
+use Zend\Db\Adapter\Adapter;
+
+class QuestionMeta
+{
+    /**
+     * @var Adapter
+     */
+    protected $adapter;
+
+    public function __construct(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    public function insert(
+        int $questionId,
+        array $array
+    ): int {
+        $sql = '
+            INSERT
+              INTO `question_meta`
+                   (`question_id`, `name`)
+            VALUES ?, ?
+                 ;
+        ';
+        $parameters = [
+            $entityId,
+            $array['name'],
+        ];
+        return (int) $this->adapter
+                          ->query($sql)
+                          ->execute($parameters)
+                          ->getGeneratedValue();
+    }
+}
