506a4ca6788f41c8e258b2332dfb1bd17e368b48
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-08-23 20:45:42 +0000

    Write sql files for question_meta and answer_meta tables

---
+++ sql/leogalle_test/answer_meta/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `answer_meta` (
+    `answer_meta_id` int(10) unsigned auto_increment,
+    `answer_id` int(10) unsigned not null,
+    PRIMARY KEY (`answer_meta_id`)
+) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ sql/leogalle_test/question_meta/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `question_meta` (
+    `question_meta_id` int(10) unsigned auto_increment,
+    `question_id` int(10) unsigned not null,
+    PRIMARY KEY (`question_meta_id`)
+) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
