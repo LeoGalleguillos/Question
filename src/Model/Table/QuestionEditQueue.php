e0c0f5513075cb20c4c4a04fee13ab610e56f5a7
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-09-12 00:36:00 +0000

    Add select and update methods to question_edit_queue table model

--- src/Model/Table/QuestionEditQueue.php
+++ src/Model/Table/QuestionEditQueue.php
@@ -1,6 +1,7 @@
 <?php
 namespace LeoGalleguillos\Question\Model\Table;
 
+use Generator;
 use Zend\Db\Adapter\Adapter;
 
 class QuestionEditQueue
@@ -56,5 +57,79 @@ class QuestionEditQueue
                     ->query($sql)
                     ->execute($parameters)
                     ->getGeneratedValue();
+    }
+
+    public function selectWhereQuestionEditQueueId(
+        int $questionEditQueueId
+    ): array {
+        $sql = '
+            SELECT `question_edit_queue_id`
+                 , `question_id`
+                 , `user_id`
+                 , `name`
+                 , `subject`
+                 , `message`
+                 , `ip`
+                 , `created`
+                 , `reason`
+                 , `queue_status_id`
+                 , `modified`
+              FROM `question_edit_queue`
+             WHERE `question_edit_queue_id` = ?
+                 ;
+        ';
+        $parameters = [
+            $questionEditQueueId,
+        ];
+        return $this->adapter->query($sql)->execute($parameters)->current();
+    }
+
+    public function selectWhereQueueStatusId(
+        int $queueStatusId
+    ): Generator {
+        $sql = '
+            SELECT `question_edit_queue_id`
+                 , `question_id`
+                 , `user_id`
+                 , `name`
+                 , `subject`
+                 , `message`
+                 , `ip`
+                 , `created`
+                 , `reason`
+                 , `queue_status_id`
+                 , `modified`
+              FROM `question_edit_queue`
+             WHERE `queue_status_id` = ?
+             ORDER
+                BY `created` ASC
+                 ;
+        ';
+        $parameters = [
+            $queueStatusId,
+        ];
+        foreach ($this->adapter->query($sql)->execute($parameters) as $array) {
+            yield $array;
+        }
+    }
+
+    public function updateSetQueueStatusIdWhereQuestionEditQueueId(
+        int $queueStatusId,
+        int $questionEditQueueId
+    ): bool {
+        $sql = '
+            UPDATE `question_edit_queue`
+               SET `question_edit_queue`.`queue_status_id` = ?
+             WHERE `question_edit_queue`.`question_edit_queue_id` = ?
+                 ;
+        ';
+        $parameters = [
+            $queueStatusId,
+            $questionEditQueueId
+        ];
+        return (bool) $this->adapter
+                           ->query($sql)
+                           ->execute($parameters)
+                           ->getAffectedRows();
     }
 }
