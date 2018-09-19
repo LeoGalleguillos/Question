c8fd3dd91851aab5129298db0eea5adb961d799b
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-09-19 03:43:46 +0000

    Write test for QuestionFromAnswer service

---
+++ test/Model/Service/QuestionFromAnswerTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace LeoGalleguillos\QuestionTest\Model\Service\Question;
+
+use LeoGalleguillos\Question\Model\Entity as QuestionEntity;
+use LeoGalleguillos\Question\Model\Factory as QuestionFactory;
+use LeoGalleguillos\Question\Model\Service as QuestionService;
+use PHPUnit\Framework\TestCase;
+
+class QuestionFromAnswerTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->questionFactoryMock = $this->createMock(
+            QuestionFactory\Question::class
+        );
+        $this->questionFromAnswerService = new QuestionService\QuestionFromAnswer(
+            $this->questionFactoryMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            QuestionService\QuestionFromAnswer::class,
+            $this->questionFromAnswerService
+        );
+    }
+
+    public function testGetQuestionFromAnswer()
+    {
+        $answerEntity = new QuestionEntity\Answer();
+        $answerEntity->setQuestionId(123);
+        $questionEntity = $this->questionFromAnswerService->getQuestionFromAnswer(
+            $answerEntity
+        );
+        $this->assertInstanceOf(
+            QuestionEntity\Question::class,
+            $questionEntity
+        );
+    }
+}
