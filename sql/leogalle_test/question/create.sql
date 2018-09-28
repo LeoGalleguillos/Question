36a546253b723b33f9fa64f5b19757c75b8ce1c4
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-09-28 14:29:45 +0000

    Add created_name and created_ip columns along with indexes

--- sql/leogalle_test/question/create.sql
+++ sql/leogalle_test/question/create.sql
@@ -7,9 +7,13 @@ CREATE TABLE `question` (
     `ip` varchar(45) default null,
     `views` int(10) unsigned NOT NULL DEFAULT '0',
     `created` datetime not null,
+    `created_name` varchar(255) default null,
+    `created_ip` varchar(45) default null,
     `deleted` datetime default null,
     PRIMARY KEY (`question_id`),
     KEY `user_id` (`user_id`),
     KEY `ip` (`ip`),
-    KEY `created_deleted` (`created`, `deleted`)
+    KEY `created_deleted` (`created`, `deleted`),
+    KEY `created_name_deleted_created` (`created_name`, `deleted`, `created`),
+    KEY `created_ip_created` (`created_ip`, `created`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
