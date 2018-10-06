077985509bcd2c27603f4d3633d9bf46d4a767f6
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-06 21:24:29 +0000

    Write preliminary test for delete answer service

---
+++ test/Model/Service/Answer/DeleteTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace LeoGalleguillos\QuestionTest\Model\Service\Answer;
+
+use Exception;
+use LeoGalleguillos\Question\Model\Entity as QuestionEntity;
+use LeoGalleguillos\Question\Model\Factory as QuestionFactory;
+use LeoGalleguillos\Question\Model\Service as QuestionService;
+use LeoGalleguillos\Question\Model\Table as QuestionTable;
+use LeoGalleguillos\User\Model\Entity as UserEntity;
+use PHPUnit\Framework\TestCase;
+
+class DeleteTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->deletedDeletedUserIdDeletedReasonTableMock = $this->createMock(
+            QuestionTable\Answer\DeletedDeletedUserIdDeletedReason::class
+        );
+        $this->deleteService = new QuestionService\Answer\Delete(
+            $this->deletedDeletedUserIdDeletedReasonTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            QuestionService\Answer\Delete::class,
+            $this->deleteService
+        );
+    }
+
+    public function testDelete()
+    {
+        $userEntity = new UserEntity\User();
+        $userEntity->setUserId(1);
+
+        $answerEntity = new QuestionEntity\Answer();
+        $answerEntity->setAnswerId(1);
+
+        $this->assertFalse(
+            $this->deleteService->delete(
+                $userEntity,
+                'reason',
+                $answerEntity
+            )
+        );
+    }
+}
