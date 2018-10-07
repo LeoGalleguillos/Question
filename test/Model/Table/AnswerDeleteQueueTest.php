2d0c41d00d1f6e9dbd7c40eedcc843fe70b4f801
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-07 04:08:15 +0000

    Write preliminary test for answer_delete_queue and question_delete_queue table models

---
+++ test/Model/Table/AnswerDeleteQueueTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace LeoGalleguillos\QuestionTest\Model\Table;
+
+use Generator;
+use LeoGalleguillos\Question\Model\Table as QuestionTable;
+use LeoGalleguillos\QuestionTest\TableTestCase;
+use Zend\Db\Adapter\Adapter;
+use PHPUnit\Framework\TestCase;
+
+class AnswerDeleteQueueTest extends TableTestCase
+{
+    /**
+     * @var string
+     */
+    protected $sqlPath;
+
+    protected function setUp()
+    {
+        $this->sqlPath = $_SERVER['PWD'] . '/sql/leogalle_test/answer_delete_queue/';
+        $configArray   = require(__DIR__ . '/../../../config/autoload/local.php');
+        $configArray   = $configArray['db']['adapters']['leogalle_test'];
+        $this->adapter = new Adapter($configArray);
+
+        $this->answerDeleteQueueTable = new QuestionTable\AnswerDeleteQueue($this->adapter);
+
+        $this->setForeignKeyChecks0();
+        $this->dropTable();
+        $this->createTable();
+        $this->setForeignKeyChecks1();
+    }
+
+    protected function dropTable()
+    {
+        $sql = file_get_contents($this->sqlPath . 'drop.sql');
+        $result = $this->adapter->query($sql)->execute();
+    }
+
+    protected function createTable()
+    {
+        $sql = file_get_contents($this->sqlPath . 'create.sql');
+        $result = $this->adapter->query($sql)->execute();
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            QuestionTable\AnswerDeleteQueue::class,
+            $this->answerDeleteQueueTable
+        );
+    }
+
+    public function testInsert()
+    {
+        $answerEditQueueId = $this->answerDeleteQueueTable->insert(
+            12345,
+            54321,
+            'reason'
+        );
+        $this->assertSame(
+            $answerEditQueueId,
+            1
+        );
+        $answerEditQueueId = $this->answerDeleteQueueTable->insert(
+            12345,
+            54321,
+            'reason'
+        );
+        $this->assertSame(
+            $answerEditQueueId,
+            2
+        );
+    }
+}
---
+++ test/Model/Table/QuestionDeleteQueueTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace LeoGalleguillos\QuestionTest\Model\Table;
+
+use Generator;
+use LeoGalleguillos\Question\Model\Table as QuestionTable;
+use LeoGalleguillos\QuestionTest\TableTestCase;
+use Zend\Db\Adapter\Adapter;
+use PHPUnit\Framework\TestCase;
+
+class QuestionDeleteQueueTest extends TableTestCase
+{
+    /**
+     * @var string
+     */
+    protected $sqlPath;
+
+    protected function setUp()
+    {
+        $this->sqlPath = $_SERVER['PWD'] . '/sql/leogalle_test/question_delete_queue/';
+        $configArray   = require(__DIR__ . '/../../../config/autoload/local.php');
+        $configArray   = $configArray['db']['adapters']['leogalle_test'];
+        $this->adapter = new Adapter($configArray);
+
+        $this->questionDeleteQueueTable = new QuestionTable\QuestionDeleteQueue($this->adapter);
+
+        $this->setForeignKeyChecks0();
+        $this->dropTable();
+        $this->createTable();
+        $this->setForeignKeyChecks1();
+    }
+
+    protected function dropTable()
+    {
+        $sql = file_get_contents($this->sqlPath . 'drop.sql');
+        $result = $this->adapter->query($sql)->execute();
+    }
+
+    protected function createTable()
+    {
+        $sql = file_get_contents($this->sqlPath . 'create.sql');
+        $result = $this->adapter->query($sql)->execute();
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            QuestionTable\QuestionDeleteQueue::class,
+            $this->questionDeleteQueueTable
+        );
+    }
+
+    public function testInsert()
+    {
+        $answerEditQueueId = $this->questionDeleteQueueTable->insert(
+            12345,
+            54321,
+            'reason'
+        );
+        $this->assertSame(
+            $answerEditQueueId,
+            1
+        );
+        $answerEditQueueId = $this->questionDeleteQueueTable->insert(
+            12345,
+            54321,
+            'reason'
+        );
+        $this->assertSame(
+            $answerEditQueueId,
+            2
+        );
+    }
+}
