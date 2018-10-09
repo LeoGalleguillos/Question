b47e4a60a2a72ed20eea9f45d3f8b64c2f0e7ffd
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-09 17:09:19 +0000

    Rename MessageDeleted to MessageCreatedDatetimeDeleted

--- src/Model/Table/Question/MessageCreatedDatetimeDeleted.php
+++ src/Model/Table/Question/MessageCreatedDatetimeDeleted.php
@@ -4,7 +4,7 @@ namespace LeoGalleguillos\Question\Model\Table\Question;
 use LeoGalleguillos\Question\Model\Table as QuestionTable;
 use Zend\Db\Adapter\Adapter;
 
-class MessageDeleted
+class MessageCreatedDatetimeDeleted
 {
     /**
      * @var Adapter
@@ -20,18 +20,22 @@ class MessageDeleted
     }
 
     /**
-     * Select where message and deleted is null.
+     * Select where message and created_datetime is greater than or equal to
+     * and deleted is null.
      *
      * @param string $message
+     * @param string $createdDateTime
      * @return array
      */
-    public function selectWhereMessageAndDeletedIsNull(
-        string $message
+    public function selectWhereMessageAndCreatedDateTimeIsGreaterThanOrEqualToAndDeletedIsNull(
+        string $message,
+        string $createdDateTime
     ): array {
         $sql = $this->questionTable->getSelect()
              .  "
               FROM `question`
              WHERE `question`.`message` = ?
+               AND `question`.`created_datetime` >= ?
                AND `question`.`deleted` IS NULL
              ORDER
                 BY `question`.`created_datetime` DESC
@@ -39,6 +43,7 @@ class MessageDeleted
         ";
         $parameters = [
             $message,
+            $createdDateTime,
         ];
         return $this->adapter->query($sql)->execute($parameters)->current();
     }
