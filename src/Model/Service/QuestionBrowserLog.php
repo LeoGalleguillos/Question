8dcb06ebd1340a3b7c8d11aa5f5766251b888791
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-29 17:30:30 +0000

    Truncate http user agent if it is too long for insert into table

--- src/Model/Service/QuestionBrowserLog.php
+++ src/Model/Service/QuestionBrowserLog.php
@@ -19,10 +19,14 @@ class QuestionBrowserLog
         QuestionEntity\Question $questionEntity
     ): bool {
         if ($this->browserService->isBrowser()) {
+            $httpUserAgent = (strlen($_SERVER['HTTP_USER_AGENT']) > 255)
+                           ? substr($_SERVER['HTTP_USER_AGENT'], 0, 255)
+                           : $_SERVER['HTTP_USER_AGENT'];
+
             $this->questionBrowserLogTable->insert(
                 $questionEntity->getQuestionId(),
                 $_SERVER['REMOTE_ADDR'],
-                $_SERVER['HTTP_USER_AGENT']
+                $httpUserAgent
             );
             return true;
         }
