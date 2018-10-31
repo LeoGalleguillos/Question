59bde3673563c604298d44ba886cf519fa7a0c33
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-10-31 22:41:21 +0000

    Add indexes on question_browser_log table

--- sql/test/question_browser_log/create.sql
+++ sql/test/question_browser_log/create.sql
@@ -4,5 +4,7 @@ CREATE TABLE `question_browser_log` (
     `ip` varchar(45) not null,
     `http_user_agent` varchar(255) not null,
     `created` datetime not null,
-    PRIMARY KEY (`question_browser_log_id`)
+    PRIMARY KEY (`question_browser_log_id`),
+    KEY `question_id` (`question_id`),
+    KEY `created` (`created`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
