13b01539ae7150bd2366f65de4e9132d27a55b5e
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-03 06:28:37 +0000

    Write test for question url service

---
+++ test/Model/Service/Question/UrlTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace LeoGalleguillos\QuestionTest\Model\Service;
+
+use LeoGalleguillos\Question\Model\Entity as QuestionEntity;
+use LeoGalleguillos\Question\Model\Service as QuestionService;
+use PHPUnit\Framework\TestCase;
+
+class UrlTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->rootRelativeUrlServiceMock = $this->createMock(
+            QuestionService\Question\RootRelativeUrl::class
+        );
+        $this->urlService = new QuestionService\Question\Url(
+            $this->rootRelativeUrlServiceMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            QuestionService\Question\Url::class,
+            $this->urlService
+        );
+    }
+
+    public function testGetModifiedTitle()
+    {
+        $_SERVER['HTTP_HOST'] = 'www.test.com';
+        $this->rootRelativeUrlServiceMock->method('getRootRelativeUrl')->willReturn(
+            '/questions/12345/My-Question-Title'
+        );
+
+        $questionEntity = new QuestionEntity\Question();
+
+        $this->assertSame(
+            'https://www.test.com/questions/12345/My-Question-Title',
+            $this->urlService->getUrl($questionEntity)
+        );
+    }
+}
