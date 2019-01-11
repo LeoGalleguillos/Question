0d33e39316903eda06981c96fce39d77e610ccf7
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-01-11 20:20:14 +0000

    Write Question\CreatedDeleted table model

---
+++ src/Model/Table/Question/CreatedDeleted.php
@@ -0,0 +1,56 @@
+<?php
+namespace LeoGalleguillos\Question\Model\Table\Question;
+
+use Generator;
+use LeoGalleguillos\Memcached\Model\Service as MemcachedService;
+use Zend\Db\Adapter\Adapter;
+
+class CreatedDeleted
+{
+    /**
+     * @var Adapter
+     */
+    protected $adapter;
+
+    public function __construct(
+        Adapter $adapter,
+        MemcachedService\Memcached $memcachedService
+    ) {
+        $this->adapter          = $adapter;
+        $this->memcachedService = $memcachedService;
+    }
+
+
+    public function selectQuestionIdWhereCreatedInYearAndDeletedIsNull(int $year): array
+    {
+        $cacheKey = md5(__METHOD__);
+        if (null !== ($questionIds = $this->memcachedService->get($cacheKey))) {
+            return $questionIds;
+        }
+
+        $sql = '
+            SELECT `question_id`
+              from question
+             where created >= ?
+               and created < ?
+               AND deleted IS NULL
+
+             ORDER
+                BY views DESC
+
+             LIMIT 100
+                 ;
+        ';
+        $parameters = [
+            "$year-01-01 05:00:00",
+            "{$year + 1}-01-01 05:00:00",
+        ];
+        $questionIds = [];
+        foreach ($this->adapter->query($sql)->execute($parameters)->current() as $array) {
+            $questionIds[] = (int) $array['question_id'];
+        }
+
+        $this->memcachedService->setForDays($cacheKey, $questionIds, 14);
+        return $questionIds;
+    }
+}
