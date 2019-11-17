25fc63c53b046d2a0698e4c1f9a3ee0c0238483f
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-11-17 08:39:56 +0000

    Improve SQL style

--- src/Model/Table/Question/CreatedIpDeletedDatetimeDeletedUserId.php
+++ src/Model/Table/Question/CreatedIpDeletedDatetimeDeletedUserId.php
@@ -24,9 +24,9 @@ class CreatedIpDeletedDatetimeDeletedUserId
         $sql = '
             SELECT COUNT(*) AS `count`
               FROM `question`
-             WHERE created_ip = ?
-               AND `deleted_datetime` > utc_timestamp() - INTERVAL 1 DAY
-               AND deleted_user_id = 0
+             WHERE `created_ip` = ?
+               AND `deleted_datetime` > UTC_TIMESTAMP() - INTERVAL 1 DAY
+               AND `deleted_user_id` = 0
                  ;
         ';
         $parameters = [
