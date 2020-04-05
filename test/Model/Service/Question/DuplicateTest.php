d7787268e4d1072faefcc954917bad57acbb0c81
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-04-05 07:10:28 +0000

    Write unit test for duplicate question service

---
+++ test/Model/Service/Question/DuplicateTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace LeoGalleguillos\QuestionTest\Model\Service\Question;
+
+use Exception;
+use Laminas\Db\Adapter\Driver\Pdo\Result;
+use LeoGalleguillos\Question\Model\Entity as QuestionEntity;
+use LeoGalleguillos\Question\Model\Factory as QuestionFactory;
+use LeoGalleguillos\Question\Model\Service as QuestionService;
+use LeoGalleguillos\Question\Model\Table as QuestionTable;
+use LeoGalleguillos\Test\Hydrator as TestHydrator;
+use PHPUnit\Framework\TestCase;
+
+class DuplicateTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->questionFactoryMock = $this->createMock(
+            QuestionFactory\Question::class
+        );
+        $this->messageDeletedDatetimeCreatedDatetimeTableMock = $this->createMock(
+            QuestionTable\Question\MessageDeletedDatetimeCreatedDatetime::class
+        );
+        $this->duplicateService = new QuestionService\Question\Duplicate(
+            $this->questionFactoryMock,
+            $this->messageDeletedDatetimeCreatedDatetimeTableMock
+        );
+    }
+
+    public function test_getDuplicate_emptyResult_throwsException()
+    {
+        $this->expectException(Exception::class);
+        $this->duplicateService->getDuplicate('this is the message');
+    }
+
+    public function test_getDuplicate_oneResult_oneQuestionEntity()
+    {
+        $resultHydrator = new TestHydrator\Result();
+        $resultMock = $this->createMock(
+            Result::class
+        );
+        $resultHydrator->hydrate(
+            $resultMock,
+            [
+                [
+                    'question_id' => '12345',
+                ]
+            ]
+        );
+
+        $questionEntity = new QuestionEntity\Question();
+
+        $this->messageDeletedDatetimeCreatedDatetimeTableMock
+            ->expects($this->once())
+            ->method('selectWhereMessageAndDeletedDatetimeIsNullOrderByCreatedDatetimeDescLimit1')
+            ->with('this is the message')
+            ->willReturn($resultMock);
+        $this->questionFactoryMock
+            ->expects($this->once())
+            ->method('buildFromArray')
+            ->with(['question_id' => '12345'])
+            ->willReturn($questionEntity);
+
+        $this->assertSame(
+            $questionEntity,
+            $this->duplicateService->getDuplicate('this is the message')
+        );
+    }
+}
