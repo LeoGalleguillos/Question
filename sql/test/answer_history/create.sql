c119510957a006052f87fbe4f563f69544097892
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-04-17 22:34:55 +0000

    Enable `answer_history`.`created` to be null because some historic values are unknown

--- sql/test/answer_history/create.sql
+++ sql/test/answer_history/create.sql
@@ -4,7 +4,7 @@ CREATE TABLE `answer_history` (
     `name` varchar(255) default null,
     `message` text,
     `modified_reason` varchar(255) DEFAULT NULL,
-    `created` datetime not null,
+    `created` datetime DEFAULT NULL,
     PRIMARY KEY (`answer_history_id`),
     KEY `answer_id_created` (`answer_id`, `created`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
