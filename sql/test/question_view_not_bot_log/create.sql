ffd70da5ab4a99bd9485001a1146e6cc5d22229a
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-05-28 17:12:23 +0000

    Add key and foreign key on `question_view_not_bot_log`.`question_id` column

--- sql/test/question_view_not_bot_log/create.sql
+++ sql/test/question_view_not_bot_log/create.sql
@@ -5,5 +5,10 @@ CREATE TABLE `question_view_not_bot_log` (
     `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`question_view_not_bot_log_id`),
     KEY `created_question_id_ip` (`created`, `question_id`, `ip`),
-    KEY `ip` (`ip`)
+    KEY `question_id` (`question_id`),
+    KEY `ip` (`ip`),
+    CONSTRAINT FOREIGN KEY (`question_id`)
+        REFERENCES `question` (`question_id`)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
