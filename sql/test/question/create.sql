cbdc7c0fc98aff17fe3e5623b148b836d6112f5c
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-06-08 04:50:47 +0000

    Drop `question`.`views_browser` column

--- sql/test/question/create.sql
+++ sql/test/question/create.sql
@@ -4,7 +4,6 @@ CREATE TABLE `question` (
     `subject` varchar(255) not null,
     `message` text,
     `views` int(10) unsigned NOT NULL DEFAULT '0',
-    `views_browser` int(10) unsigned NOT NULL DEFAULT '0',
     `views_not_bot_one_month` int(10) unsigned NOT NULL DEFAULT '0',
     `created_datetime` datetime NOT NULL,
     `created_name` varchar(255) default null, #@todo Rename column to `name`
@@ -17,10 +16,7 @@ CREATE TABLE `question` (
     `deleted_reason` varchar(255) default null,
     PRIMARY KEY (`question_id`),
     KEY `user_id` (`user_id`),
-    KEY `subject_deleted_datetime_views_browser` (`subject`, `deleted_datetime`, `views_browser`),
-    KEY `created_datetime_deleted_datetime_views_browser` (`created_datetime`, `deleted_datetime`, `views_browser`),
     KEY `created_name_deleted_datetime_created_datetime` (`created_name`, `deleted_datetime`, `created_datetime`),
-    KEY `created_name_deleted_datetime_views_browser` (`created_name`, `deleted_datetime`, `views_browser`),
     KEY `created_name_deleted_datetime_views_not_bot_one_month` (`created_name`, `deleted_datetime`, `views_not_bot_one_month`),
     KEY `created_ip_created_datetime` (`created_ip`, `created_datetime`),
     KEY `deleted_datetime_created_datetime` (`deleted_datetime`, `created_datetime`),
