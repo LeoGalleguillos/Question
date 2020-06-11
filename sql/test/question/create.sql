c2fcf7c4ad8d7601bb94bd9e5b928ee83beec006
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-06-11 19:55:08 +0000

    Add index on (`created_datetime`, `deleted_datetime`, `views_not_bot_one_month`) columns

--- sql/test/question/create.sql
+++ sql/test/question/create.sql
@@ -19,6 +19,7 @@ CREATE TABLE `question` (
     `deleted_reason` varchar(255) default null,
     PRIMARY KEY (`question_id`),
     KEY `user_id` (`user_id`),
+    KEY `created_datetime_deleted_datetime_views_not_bot_one_month` (`created_datetime`, `deleted_datetime`, `views_not_bot_one_month`),
     KEY `created_name_deleted_datetime_created_datetime` (`created_name`, `deleted_datetime`, `created_datetime`),
     KEY `created_name_deleted_datetime_views_not_bot_one_month` (`created_name`, `deleted_datetime`, `views_not_bot_one_month`),
     KEY `created_ip_created_datetime` (`created_ip`, `created_datetime`),
