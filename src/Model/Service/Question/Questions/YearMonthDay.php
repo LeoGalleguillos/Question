2444f466e4cf76155a1958d5e98c150ce9a62c08
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-06-11 19:55:46 +0000

    Write hotfix so that query for getting questions for specific day is performant

--- src/Model/Service/Question/Questions/YearMonthDay.php
+++ src/Model/Service/Question/Questions/YearMonthDay.php
@@ -38,20 +38,25 @@ class YearMonthDay
         $dateTimeMax->add(new DateInterval('P1D'))
             ->sub(new DateInterval('PT1S'));
 
-        $select = $this->sql
-            ->select('question')
-            ->columns($this->questionTable->getSelectColumns())
-            ->where([
-                new LaminasDb\Sql\Predicate\Between(
-                    'created_datetime',
-                    $dateTimeMin->format('Y-m-d H:i:s'),
-                    $dateTimeMax->format('Y-m-d H:i:s')
-                ),
-                'deleted_datetime' => null,
-            ])
-            ->order('views_not_bot_one_month DESC')
-            ;
-        $result = $this->sql->prepareStatementForSqlObject($select)->execute();
+        $sql = "
+            SELECT `question`.`question_id` AS `question_id`, `question`.`user_id` AS `user_id`, `question`.`subject` AS `subject`, `question`.`message` AS `message`, `question`.`views` AS `views`, `question`.`views_not_bot_one_month` AS `views_not_bot_one_month`, `question`.`created_datetime` AS `created_datetime`, `question`.`created_name` AS `created_name`, `question`.`created_ip` AS `created_ip`, `question`.`modified_user_id` AS `modified_user_id`, `question`.`modified_datetime` AS `modified_datetime`, `question`.`modified_reason` AS `modified_reason`, `question`.`deleted_datetime` AS `deleted_datetime`, `question`.`deleted_user_id` AS `deleted_user_id`, `question`.`deleted_reason` AS `deleted_reason`
+
+              FROM `question`
+
+             FORCE
+             INDEX (`created_datetime_deleted_datetime_views_not_bot_one_month`)
+
+             WHERE `created_datetime` BETWEEN ? AND ?
+               AND `deleted_datetime` IS NULL
+
+             ORDER
+                BY `views_not_bot_one_month` DESC
+        ";
+        $parameters = [
+            $dateTimeMin->format('Y-m-d H:i:s'),
+            $dateTimeMax->format('Y-m-d H:i:s'),
+        ];
+        $result = $this->sql->getAdapter()->query($sql)->execute($parameters);
 
         foreach ($result as $array) {
             yield $this->questionFactory->buildFromArray($array);
